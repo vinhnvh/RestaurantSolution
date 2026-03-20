5573f3d4780301c6f7e0be009940e63e8ef29153
⫸S.a.z⫷ <vinhnvh@gmail.com>
2026-03-20 22:07:26 +0700

    Hoan thanh UserControl TableControl mo Form theo index

---
+++ Restaurant.UI/TableControl.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Restaurant.UI
+{
+    partial class TableControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnTable = new Button();
+            SuspendLayout();
+            // 
+            // btnTable
+            // 
+            btnTable.Dock = DockStyle.Fill;
+            btnTable.Location = new Point(0, 0);
+            btnTable.Name = "btnTable";
+            btnTable.Size = new Size(150, 150);
+            btnTable.TabIndex = 0;
+            btnTable.Text = "button1";
+            btnTable.UseVisualStyleBackColor = true;
+            btnTable.Click += btnTable_Click;
+            // 
+            // TableControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnTable);
+            Name = "TableControl";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnTable;
+    }
+}
---
+++ Restaurant.UI/TableControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace Restaurant.UI
+{
+    public partial class TableControl : UserControl
+    {
+        // 1. Kho chứa dữ liệu
+        private int _formIndex;
+
+        // 2. Cửa sổ giao tiếp với bảng Properties
+        [Category("Cấu Hình Bàn")]
+        [Description("Số thứ tự để mở Form tương ứng (1-6)")]
+        public int FormIndex
+        {
+            get { return _formIndex; }
+            set
+            {
+                _formIndex = value;
+                // Khi bạn gõ số ở ngoài, chữ trên nút sẽ đổi theo ngay
+                if (btnTable != null)
+                {
+                    btnTable.Text = "Bàn số " + _formIndex;
+                }
+            }
+        }
+
+        public TableControl()
+        {
+            InitializeComponent();
+        }
+
+        // 3. Xử lý khi click vào nút
+        private void btnTable_Click(object sender, EventArgs e)
+        {
+            switch (_formIndex)
+            {
+                case 1: new Form1().Show(); break;
+                case 2: new Form2().Show(); break;
+                case 3: new Form3().Show(); break;
+                case 4: new Form4().Show(); break;
+                case 5: new Form5().Show(); break;
+                case 6: new Form6().Show(); break;
+                default:
+                    MessageBox.Show("Vui lòng gán FormIndex từ 1 đến 6 trong bảng Properties!");
+                    break;
+            }
+        }
+    }
+}
